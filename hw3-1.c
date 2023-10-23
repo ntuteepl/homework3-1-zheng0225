14b082dedc25fc46e94336e84535aa568cbf7b8b
zheng0225 <146065408+zheng0225@users.noreply.github.com>
2023-10-23 18:14:00 +0800

    Update hw3-1.c

--- hw3-1.c
+++ hw3-1.c
@@ -1,1 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main()
+{
+int num,a;
+scanf("%d",&num);
+for(a=2;a<num-1;a++)
+{
+    if (num%a==0)
+    {
+           printf("no");
+          break;
+    }
+    else{
+
+        printf("yes");
+    }
+return 0;
+}
+
+
+
+
+
+
+
+
+
+}
